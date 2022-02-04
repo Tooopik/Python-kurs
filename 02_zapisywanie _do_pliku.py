08628c4afa609517e8926d87de9530901baefa55
Adrian Wójcik <adrian.wojcik00@gmail.com>
2022-02-04 13:50:24 +0100

    First commit on 04.02.2022

--- 02_zapisywanie _do_pliku.py
+++ 02_zapisywanie _do_pliku.py
@@ -38,3 +38,19 @@ with open('techs.txt', 'r') as file:
 print(technologies)
 
 # %%
+gwiazdki = {2: 8, 4: 7, 6: 6, 8: 5, 10: 4, 12: 3, 14: 2, 16: 1, 18: 0}
+idx = 0
+
+with open('tree.txt', 'w') as file:
+    while idx != 2:
+        print('', file=file)
+        for key in gwiazdki:
+            print(' ' * gwiazdki[key], '*' * key, file=file)
+        idx += 1
+
+# %%
+with open('tree2.txt', 'w') as file:
+    for j in range(2):
+        for i in range(10):
+            print('{:>9}'.format('*'*i), end='', file=file)
+            print('{}'.format('*'*i), file=file)
---
+++ CW3_03.py
@@ -0,0 +1,8 @@
+# %%
+tech = ['python', 'java', 'sql', 'sas']
+
+with open('techs.txt', 'w') as file:
+    for item in tech:
+        print(item, file=file)
+
+# %%
