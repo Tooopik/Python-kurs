f1f99a7890b5f7cbdbfee87a36cfc5c8e4024ef3
Adrian Wójcik <adrian.wojcik00@gmail.com>
2022-03-23 20:24:42 +0100

    Add some files

--- 200+Cwiczen_Python od A do Z/---INPUT/OUTPUT---/1.Wczytywanie plików/CW_113.py
+++ 200+Cwiczen_Python od A do Z/---INPUT/OUTPUT---/1.Wczytywanie plików/CW_113.py
@@ -7,3 +7,11 @@ for data in content[1:]:
     result.append(int(data.split(',')[-1]))
 print(f'Max Vol: {max(result)}')
 # %%
+# ------------------------------------------------------------------
+with open('plw_d.csv', 'r') as file:
+    content = file.read().splitlines()
+
+volumes = [line.split(',')[-1] for line in content][1:]
+volumes = [int(vol) for vol in volumes]
+max_vol = max(volumes)
+print(f'Max Vol: {max_vol}')
