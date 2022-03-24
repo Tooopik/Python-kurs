96d00ffbab3daa7cde90a9bfd0a5164c27b6daf7
Adrian Wójcik <adrian.wojcik00@gmail.com>
2022-03-24 22:55:18 +0100

    Add some files

---
+++ 200+Cwiczen_Python od A do Z/---FUNKCJE---/1.Funkcje wbudowane/CW_118.py
@@ -0,0 +1,7 @@
+# %%
+x = -1.5
+expression = 'x**2 + x'
+
+print(eval(expression))
+
+# %%
---
+++ 200+Cwiczen_Python od A do Z/---FUNKCJE---/1.Funkcje wbudowane/CW_119.py
@@ -0,0 +1,14 @@
+# %%
+var1 = 'Python'
+var2 = ('Python')
+var3 = ('Python',)
+var4 = ['Python']
+var5 = {'Python'}
+
+print(isinstance(var1, tuple))
+print(isinstance(var2, tuple))
+print(isinstance(var3, tuple))
+print(isinstance(var4, tuple))
+print(isinstance(var5, tuple))
+
+# %%
